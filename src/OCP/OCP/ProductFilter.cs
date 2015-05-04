e33c87674c24e870fa539de62e3b46ea224bdbb8
Muhammad Afzal <afzal.qureshi@outlook.com>
2015-05-04 12:15:53 +0530

    Added two more filters voilation of OCP

--- src/OCP/OCP/ProductFilter.cs
+++ src/OCP/OCP/ProductFilter.cs
@@ -5,9 +5,38 @@
 
     public class ProductFilter
     {
+        /// <summary>
+        /// Filter the products by color.
+        /// </summary>
+        /// <param name="products">The products.</param>
+        /// <param name="productColor">Color of the product.</param>
+        /// <returns></returns>
         public IEnumerable<Product> ByColor(IList<Product> products, string productColor)
         {
             return products.Where(product => product.Color == productColor);
+        }
+
+        /// <summary>
+        /// Filter the products by size.
+        /// </summary>
+        /// <param name="products">The products.</param>
+        /// <param name="productSize">Size of the product.</param>
+        /// <returns></returns>
+        public IEnumerable<Product> BySize(IList<Product> products, string productSize)
+        {
+            return products.Where(product => (product.Size == productSize));
+        }
+
+        /// <summary>
+        /// Filter the products by size and color 
+        /// </summary>
+        /// <param name="products">The products.</param>
+        /// <param name="productColor">Color of the product.</param>
+        /// <param name="productSize">Size of the product.</param>
+        /// <returns></returns>
+        public IEnumerable<Product> ByColorAndSize(IList<Product> products, string productColor, string productSize)
+        {
+            return products.Where(product => (product.Color == productColor) && (product.Size == productSize));
         }
     }
 }
