a6cef6daa96ea4b1510688d89bf95bb9c76d3325
Muhammad Afzal <afzal.qureshi@outlook.com>
2015-05-04 12:05:39 +0530

    Added the first product filter

---
+++ src/OCP/OCP/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace OCP
+{
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the color.
+        /// </summary>
+        /// <value>The color.</value>
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the size.
+        /// </summary>
+        /// <value>The size.</value>
+        public string Size
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/OCP/OCP/ProductFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace OCP
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> ByColor(IList<Product> products, string productColor)
+        {
+            return products.Where(product => product.Color == productColor);
+        }
+    }
+}
